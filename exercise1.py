427f11965925efa75e984371bc3d2d6f1a314d22
GurpreetThind <thind.cse@gmail.com>
2020-02-19 18:02:40 -0500

    Completed Chapter 1 19 Feb 20

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+# - *- coding: utf- 8 - *- 
+
+print "another line"
+print "only one of the lines"
+print '"#" comments out the line'
+#print "nothing will be printed"
+print "removing '#' something will be printed"
